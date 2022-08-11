b903be0bbf20e292d0dae4e9114ac734f8c590dc
morch23mj <106665902+morch23mj@users.noreply.github.com>
2022-08-11 14:34:45 +0200

    refactor: use wetransform favicon
    Use wetransform favicon and fix config
    path.
    
    ING-3484

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'images/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
