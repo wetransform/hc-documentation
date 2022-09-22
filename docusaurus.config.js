f6452abb239628d99584e89ef5a3f74b58f59bef
Simon Templer <simon@wetransform.to>
2022-09-22 16:18:20 +0200

    feat: re-enable page edit links for documentation
    ING-3481

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,8 +36,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/wetransform/hc-documentation/tree/master/',
+          editLocalizedFiles: true
         },
         blog: {
           showReadingTime: true,
